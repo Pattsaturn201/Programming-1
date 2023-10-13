3a2802640fd8635f7ddb6d24abc55e7a6fec3178
Edwin Saa <edwin.saa@udla.edu.ec>
2023-10-13 00:01:35 -0500

    h

--- Ejercicio_1/Solucion_suma_Gauss.c
+++ Ejercicio_1/Solucion_suma_Gauss.c
@@ -4,6 +4,7 @@
 int integer=0;
 int count=1;
 int totalnum=1;
+int i;
 
 
 
@@ -11,30 +12,36 @@ int main(int argc, char const *argv[])
 {
     printf("Welcome to the Gauss sum\n" );
     printf("Enter an integer number\n");
-    scanf("%d",integer);
+    scanf("%d",&integer);
 
-    do while (integer < 0)
+    do 
     {
 
-       if (integer < 0)
+       if (integer > 0)
        {
          for ( i = 1; i <= integer; i++)
          {
-            printf("%d",i);
+            printf("%d\n",i);
             count = i;
             totalnum += count;
-            printf("%d",totalnum);
+            printf("%d\n",totalnum);
             
          }
          
-        printf("your result of the Glauss sum") 
+        printf("your result of the Glauss sum:%d\n",totalnum); 
         
        }
+      else
+      {
+        printf("your have entered a incorrect kind of number (only positive integers)\n"); 
+      }
+      
         
-    }
+    } while (integer < 0);
     
     return 0;
 }
+
 
 
 
