194577c22fe8f0fc0fb025450bf4e5543900e56f
Pattsaturn201 <122304019+Pattsaturn201@users.noreply.github.com>
2023-10-17 01:55:50 -0500

    Add files via upload

---
+++ week 4/deber_s3_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//mostrar todos los enteros comprendidos entre el 1 y el 100
+
+const int counter=100;
+
+int main(int argc, char const *argv[])
+{
+    int i;
+
+    for (i = 1; i <= counter  ; i++) //
+    {
+        printf("%d\n",i);//
+    }
+
+    return 0;
+}
